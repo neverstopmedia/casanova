4dabc67fa73e0b24d3bbe785e7fb1f302bdf9376
abed-shatila <88887911+abed-shatila@users.noreply.github.com>
2022-09-30 11:33:40 +0400

    1.0.7

--- functions.php
+++ functions.php
@@ -27,7 +27,7 @@ class Casanova{
     private function define_constants(){
         define( 'CASANOVA_DIR', get_template_directory() );
         define( 'CASANOVA_URI', get_template_directory_uri() );
-        define( 'CASANOVA_VERSION', '1.0.6' );
+        define( 'CASANOVA_VERSION', '1.0.7' );
         define( 'CASANOVA_API_VERSION', '1' );
         define( 'CASANOVA_API_ROUTE', 'casanova/v'.CASANOVA_API_VERSION );
     }
--- inc/misc/scripts.php
+++ inc/misc/scripts.php
@@ -5,7 +5,7 @@
  * @since 1.0.0
  */
 function casanova_enqueue_admin_scripts(){
-	wp_enqueue_style( 'casanova-admin', CASANOVA_URI . '/assets/css/admin.css', [], '1.0.0' );
+	wp_enqueue_style( 'casanova-admin', CASANOVA_URI . '/assets/css/admin.css', [], CASANOVA_VERSION );
 }
 add_action( 'admin_enqueue_scripts', 'casanova_enqueue_admin_scripts' );
 
@@ -20,7 +20,7 @@ function casanova_conditional_admin_queue( $hook ) {
     if ( $hook == 'post.php' ) {
 
         if ( $post->post_type == 'list' || $post->post_type == 'casino' )
-		wp_enqueue_script( 'casanova', CASANOVA_URI . '/assets/js/casanova.js', ['jquery', 'acf-input'] , '', true );
+		wp_enqueue_script( 'casanova', CASANOVA_URI . '/assets/js/casanova.js', ['jquery', 'acf-input'] , CASANOVA_VERSION, true );
         
     }
 
